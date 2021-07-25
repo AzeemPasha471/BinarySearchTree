b301379e45a6f9d0b849fa7c9fcc0a307dea4b14
Azeempasha471 <abdulazeem1023@gmail.com>
2021-07-25 18:41:46 +0530

    Added:CreatedBinaryTree

--- com/binarytree/MainTree.java
+++ com/binarytree/MainTree.java
@@ -1,9 +1,19 @@
 package com.binarytree;
 
+
 public class MainTree {
 
 	public static void main(String[] args) {
-		System.out.println("welocme to binary tree");
+		//creating instances
+				BinaryTree bst = new BinaryTree();
+				Node root = null;
+				/*Inserting node in the Binary tree*/
+				
+				root = bst.insert(root, 56);
+				root = bst.insert(root, 30);
+				root = bst.insert(root, 70);
+				bst.print(root);
+				}
 	}
 
-}
+
